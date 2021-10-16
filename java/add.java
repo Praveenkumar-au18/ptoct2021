d4dd82254ef082b5bf823ad02814194234e329e9
Praveenkumar-au18 <praveenkumar.au18@bitsathy.ac.in>
2021-10-16 07:09:37 +0000

    commit1

---
+++ java/add.java
@@ -0,0 +1,16 @@
+class add
+{
+        public static void main(String []args)
+        {
+                int a=5,b=10,c;
+                c=a+b;
+                int d=Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+                float e=Float.parseFloat(args[2])+Float.parseFloat(args[3]);
+                System.out.println("constant: "+ c);
+                System.out.println("Integer: "+ (Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+                System.out.println("Float: "+ (Float.parseFloat(args[2])+Float.parseFloat(args[3])));
+                System.out.println(d);
+                System.out.println(e);
+                System.out.println(d+" "+e);
+        }
+}
